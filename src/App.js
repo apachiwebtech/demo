6345c4a4a276d3c99afdd7e1189e0a6d75d88238
apache <148771971+apachiwebtech@users.noreply.github.com>
2024-01-30 11:46:18 +0530

    file added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Satyam</p>
+      <h2>
+        Satkar
+      </h2>
+    </div>
+  );
+}
+
+export default App;
